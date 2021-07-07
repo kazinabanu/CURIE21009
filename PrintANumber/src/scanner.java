518eb7f09b6b044340cc963b4bddf9924fd44a68
Kazina <kasina.banu@gmail.com>
2021-07-07 23:57:12 +0530

    upload

---
+++ PrintANumber/src/scanner.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class scanner
+{
+
+	public static void main(String[] args) {
+		
+		Scanner reader = new Scanner(System.in);
+		System.out.println("Enter a number:");
+		int number = reader.nextInt();
+		System.out.println("You Entered:"+number);
+		
+
+	}
+
+}
