71424e26022ae75c511c73859244a748b63da139
Kazina <kasina.banu@gmail.com>
2021-07-07 23:54:39 +0530

    upload

---
+++ ASCII value/src/asciivalue/Basic5.java
@@ -0,0 +1,17 @@
+package asciivalue;
+
+public class Basic5 {
+
+	public static void main(String[] args) {
+		
+		char ch= 'H';
+		int ascii=ch;
+		int castascii=(int) ch;
+		System.out.println("The ASCII value of " + ch + " is: " + ascii);
+		System.out.println("The ASCII value of " + ch + " is: " + castascii);
+		
+		
+
+	}
+
+}
